df383480136486306f68680012f1d17daab03412
Eric Chen <eric_chen@g.harvard.edu>
2026-06-16 21:55:16 +0000

    moved all the parameters hardcoded in run_training.py to the JSON file.

--- scripts/run_training.py
+++ scripts/run_training.py
@@ -16,6 +16,8 @@ with open(config_path) as f:
     unet_config = json.load(f)
 
 in_channels = unet_config["in_channels"]
+print("in_channels:", in_channels)
+print("in_channels shape:", type(in_channels))
 num_fmaps = unet_config["num_fmaps"]
 fmap_inc_factor = unet_config["fmap_inc_factor"]
 downsample_factors = eval(
@@ -35,6 +37,35 @@ padding = unet_config["padding"]
 constant_upsample = unet_config["constant_upsample"]
 neighborhood = unet_config["outputs"]["3d_affs"]["neighborhood"]
 boundary = unet_config["outputs"]["3d_affs"]["grow_boundary"]
+
+ 
+input_dir = unet_config['input_dir']
+output_dir = unet_config['output_dir']
+n_training_steps = unet_config['n_training_steps'], 
+
+print(n_training_steps)
+print(type(n_training_steps))
+input_shapes = unet_config['input_shape'] #watch out that the global variable is plural but inside the json and fxn the arg is singular
+output_shapes = unet_config['output_shape'] #watch out that the global variable is plural but inside the json and fxn the arg is singular
+batch_size = unet_config['batch_size']
+prob_augment = unet_config['prob_augment']
+var_noise = unet_config['var_noise']
+save_checkpoints_every = unet_config['save_checkpoints_every']
+save_snapshots_every = unet_config['save_snapshots_every']
+sparse_mask = eval(unet_config['sparse_mask'])
+rotate_aug = eval(unet_config['rotate_aug'])
+log_wandb = eval(unet_config['log_wandb'])
+wandb_project = unet_config['wandb_project']
+
+# if wandb_project is not None:
+#     wandb_project == eval(wandb_project)
+# else: 
+#     pass
+#currently expecting wandb_project to pass in None. 
+# if you pass in a string instead, it will just pass so it stays a string. 
+
+wandb_run_name = unet_config['wandb_run_name']
+log_every = unet_config['log_every']
 
 
 #initialize the Unet with the correct parameters, from the config file. 
@@ -62,20 +93,20 @@ train(
     input_dir = '/mnt/efs/dl_jrc/student_data/S-EC/diam300/',
     output_dir = '/mnt/efs/dl_jrc/student_data/S-EC/diam300/model_outputs/',
     config_path = config_path,
-    n_training_steps = 10000, 
-    input_shape = [1, 64, 64, 64],
-    output_shape = [1, 44, 24, 24],
-    batch_size = 1,
-    prob_augment = 0.3, 
-    var_noise = 10e-5,
+    n_training_steps = n_training_steps, 
+    input_shape = input_shapes,#[1, 64, 64, 64],
+    output_shape = output_shapes,#[1, 44, 24, 24],
+    batch_size = batch_size,#1,
+    prob_augment = prob_augment,#0.3, 
+    var_noise = var_noise,#10e-5,
     neighborhood = neighborhood,
-    save_snapshots_every = 100,
-    save_checkpoints_every = 100,
-    sparse_mask = True,
-    rotate_aug = False,
-    log_wandb = True,
-    wandb_project = "3DSegStuff",
-    wandb_run_name = None,
-    log_every = 1,
+    save_snapshots_every = save_snapshots_every, #100,
+    save_checkpoints_every = save_checkpoints_every, #100,
+    sparse_mask = sparse_mask, #True,
+    rotate_aug = rotate_aug, #False,
+    log_wandb = log_wandb, #True,
+    wandb_project = wandb_project, #"3DSegStuff",
+    wandb_run_name = wandb_run_name, #None,
+    log_every = log_every,
     unet_config = unet_config,
     boundary = boundary) # contents of the config dict, for saving to WanDB
