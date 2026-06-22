67deb15f4b4da7f29bf56fb16df8e76d16e1bfbe
MorganeStorey <morgane.storey@epfl.ch>
2026-06-22 13:47:26 +0000

    Keep 2D-affs models, checkpoints, and configs (branch only)

---
+++ src/ThreeD_affs_from_2d_affs_long_range/predict.py
@@ -0,0 +1,192 @@
+import gunpowder as gp
+import json
+import logging
+import os
+import click
+
+from funlib.geometry import Coordinate, Roi
+from funlib.persistence import open_ds
+from funlib.persistence import prepare_ds
+
+import torch
+
+from ThreeD_affs_from_2d_affs_long_range.model import AffsUNet
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+setup_dir = os.path.abspath(os.path.dirname(os.path.realpath(__file__)))
+
+
+@click.command()
+@click.option(
+    "--checkpoint",
+    "-c",
+    required=True,
+    type=click.Path(file_okay=True, dir_okay=False),
+    help="Path to checkpoint file",
+)
+@click.option(
+    "--input_datasets",
+    "-i",
+    required=True,
+    multiple=True,
+    type=click.Path(exists=True, file_okay=False, dir_okay=True),
+    help="Path to input zarr datasets",
+)
+@click.option(
+    "--output_datasets",
+    "-o",
+    required=True,
+    multiple=True,
+    type=click.Path(),
+    help="Path to output zarr datasets",
+)
+@click.option(
+    "--roi_offset",
+    "-ro",
+    type=str,
+    help="Offset of ROI in world units (space separated integers)",
+)
+@click.option(
+    "--roi_shape",
+    "-rs",
+    type=str,
+    help="Shape of ROI in world units (space separated integers)",
+)
+@click.option("--num_workers", "-n", default=1, type=int, help="Number of workers")
+@click.option(
+    "--daisy", "-d", default=False, is_flag=True, help="Use daisy for parallelization"
+)
+def predict(
+    checkpoint,
+    input_datasets,
+    output_datasets,
+    roi_offset,
+    roi_shape,
+    num_workers,
+    daisy,
+):
+    output_dataset = output_datasets[0]
+
+    # load net config
+    with open(os.path.join(setup_dir, "net_config.json")) as f:
+        logging.info(
+            "Reading setup config from %s" % os.path.join(setup_dir, "net_config.json")
+        )
+        net_config = json.load(f)
+
+    #################
+    
+    neighborhood = net_config["outputs"]["3d_affs"]["neighborhood"]
+    input_arr = open_ds(input_datasets[0])
+    output_shape = net_config["output_shape"]
+    print(input_arr.shape) 
+
+    # Make empty zarr
+    output_arr = prepare_ds(
+        output_dataset,
+        shape=(len(neighborhood), *input_arr.shape[1:]), #affinity has 3 channels
+        voxel_size=input_arr.voxel_size, 
+        offset=input_arr.offset,
+        axis_names=input_arr.axis_names,
+        units=input_arr.units,
+        types=input_arr.types,
+        chunk_shape=output_shape,
+        dtype=input_arr.dtype
+        )
+
+    predict_request = gp.BatchRequest()
+
+    #################
+
+    shape_increase = net_config["shape_increase"]
+    input_shape = [x + y for x, y in zip(shape_increase, net_config["input_shape"])]
+    output_shape = [x + y for x, y in zip(shape_increase, net_config["output_shape"])]
+    
+    input_affs_ds = open_ds(input_datasets[0], "r")
+
+    voxel_size = input_affs_ds.voxel_size
+    input_size = Coordinate(input_shape) * voxel_size
+    output_size = Coordinate(output_shape) * voxel_size
+
+    if not daisy:
+        logging.info("Using Scan node with %d workers" % num_workers)
+        if roi_offset is not None:
+            roi_offset = Coordinate(roi_offset.split(" "))
+            roi_shape = Coordinate(roi_shape.split(" "))
+            roi = Roi(roi_offset, roi_shape).snap_to_grid(voxel_size, mode="grow")
+        else:
+            roi = input_affs_ds.roi
+
+    model = AffsUNet()
+
+    # load checkpoint
+    checkpoint_path = checkpoint if os.path.exists(checkpoint) else f"{checkpoint}.ckpt"
+    if not os.path.exists(checkpoint_path):
+        raise FileNotFoundError(f"Neither {checkpoint} nor {checkpoint}.ckpt were found.")
+
+    # Load the checkpoint
+    state_dict = torch.load(checkpoint_path, map_location='cpu')
+    state_dict = state_dict.get('state_dict', state_dict.get('model_state_dict', state_dict))
+    state_dict = {k.removeprefix('model.'): v for k, v in state_dict.items()}
+
+    model.load_state_dict(state_dict)
+    model.eval()
+
+    input_affs = gp.ArrayKey("INPUT_AFFS")
+    pred_affs = gp.ArrayKey("PRED_AFFS")
+
+    chunk_request = gp.BatchRequest()
+    chunk_request.add(input_affs, input_size)
+    chunk_request.add(pred_affs, output_size)
+
+    source = (
+        gp.ArraySource(input_affs, input_affs_ds, True),
+    ) + gp.MergeProvider()
+
+    predict = gp.torch.Predict(
+        model,
+        inputs={
+            "input_affs": input_affs
+        },
+        outputs={
+            0: pred_affs,
+        },
+        array_specs={pred_affs: gp.ArraySpec(roi=roi)} if not daisy else None,
+    )
+
+    if daisy:
+        scan = gp.DaisyRequestBlocks(
+            chunk_request,
+            roi_map={
+                input_affs: "read_roi",
+                pred_affs: "write_roi",
+            },
+        )
+    else:
+        scan = gp.Scan(chunk_request, num_workers=num_workers)
+
+    write = gp.ZarrWrite(
+        dataset_names={pred_affs: output_dataset.split(".zarr")[-1]},
+        store=output_dataset.split(".zarr")[0] + ".zarr",
+    )
+
+    pipeline = (
+        source
+        + gp.Normalize(input_affs)
+        + gp.Pad(input_affs, None, mode="reflect")
+        + gp.Unsqueeze([input_affs])
+        + predict
+        + gp.Squeeze([pred_affs])
+        + gp.IntensityScaleShift(pred_affs, 255, 0)
+        + write
+        + scan
+    )
+
+    with gp.build(pipeline):
+        pipeline.request_batch(predict_request)
+
+
+if __name__ == "__main__":
+    predict()
